76d085334f6475ef6fe649646164858f077f75aa
you.yamazaki <youyou0306@gmail.com>
2023-01-16 00:13:03 +0900

    ShowServlet.javaを作成

--- src/controllers/ShowServlet.java
+++ src/controllers/ShowServlet.java
@@ -2,6 +2,7 @@ package controllers;
 
 import java.io.IOException;
 
+import javax.persistence.EntityManager;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,20 +11,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import models.Task;
-
-
+import utils.DBUtil;
 
 /**
- * Servlet implementation class NewServlet
+ * Servlet implementation class ShowServlet
  */
-@WebServlet("/new")
-public class NewServlet extends HttpServlet {
+@WebServlet("/show")
+public class ShowServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public NewServlet() {
+    public ShowServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -32,18 +32,20 @@ public class NewServlet extends HttpServlet {
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-     // CSRF対策
-        request.setAttribute("_token", request.getSession().getId());
+        EntityManager em = DBUtil.createEntityManager();
 
-     // おまじないとしてのインスタンスを生成
-        request.setAttribute("task", new Task());
+        // 該当のIDのメッセージ1件のみをデータベースから取得
+        Task t = em.find(Task.class, Integer.parseInt(request.getParameter("id")));
 
-        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/new.jsp");
+        em.close();
+
+        // メッセージデータをリクエストスコープにセットしてshow.jspを呼び出す
+        request.setAttribute("task", t);
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/show.jsp");
         rd.forward(request, response);
-
-
-     
-
     }
+
+
 
 }
